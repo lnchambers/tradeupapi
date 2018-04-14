3ce6003a28a90822c0e1cea195d25dea60f1bbfb
Luke Chambers <lukenatchambers@gmail.com>
2018-04-14 15:10:17 -0600

    Build out institutional analytics

--- app/models/graduation.rb
+++ app/models/graduation.rb
@@ -1,4 +1,9 @@
 class Graduation < ApplicationRecord
   belongs_to :institution
   belongs_to :program
+  enum residency: ['in_state', 'out_of_state']
+
+  def self.ordered_by_year
+    order("year DESC")
+  end
 end
--- app/models/institution.rb
+++ app/models/institution.rb
@@ -6,4 +6,48 @@ class Institution < ApplicationRecord
   has_many :programs, through: :catalogs
   enum type_of_institution: ['trade_school', 'lolnuthing', 'college']
   enum tax_type: ['profit', 'not_for_profit']
+
+  def city
+    address.split(",")[1].strip
+  end
+
+  def street_address
+    address.split(",")[0].strip
+  end
+
+  def state
+    address.split[-2].strip
+  end
+
+  def zip_code
+    address.split[-1].strip
+  end
+
+  def graduation_rate
+    (enrolled.count.to_f / graduated.count.to_f).round(2)
+  end
+
+  def enrolled
+    enrolled ||= enrollments
+  end
+
+  def graduated
+    graduated ||= graduations
+  end
+
+  def graduates_per_year(year)
+    graduations.where(year: year).count
+  end
+
+  def enrollments_per_year(year)
+    enrollments.where(year: year).count
+  end
+
+  def yearly_graduates_data
+    graduations.ordered_by_year
+  end
+
+  def yearly_enrollments_data
+    enrollments.ordered_by_year
+  end
 end
