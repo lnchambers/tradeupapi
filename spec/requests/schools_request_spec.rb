a1566c5e05b9fd096a2bac7b176a6fcc76f97eb8
Luke Chambers <lukenatchambers@gmail.com>
2018-04-15 10:00:29 -0600

    Build out sort by location method

--- spec/requests/schools_request_spec.rb
+++ spec/requests/schools_request_spec.rb
@@ -4,11 +4,13 @@ describe "Schools API" do
   before :each do
     i_1 = create(:institution)
     i_2 = create(:institution, name: "Opakawagalaga Eupanifahorious")
+    i_3 = create(:institution, name: "Benedictus Cumberbund", address: "5401 20th St, Fort Collins, CO 80634")
     prog = create(:program)
     prog_2 = create(:program, name: "Bennificatris")
     Catalog.create!(institution: i_1, program: prog)
     Catalog.create!(institution: i_2, program: prog)
     Catalog.create!(institution: i_2, program: prog_2)
+    Catalog.create!(institution: i_3, program: prog_2)
   end
   it "can respond to index requests" do
     get "/api/v1/schools"
@@ -17,7 +19,13 @@ describe "Schools API" do
   end
 
   it "can respond to show requests" do
-    get "/api/v1/schools/3"
+    get "/api/v1/schools/5"
+
+    expect(response).to be_success
+  end
+
+  it "can respond to show requests" do
+    get "/api/v1/schools?location=Greeley"
 
     expect(response).to be_success
   end
