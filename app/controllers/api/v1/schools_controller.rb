130ddc55640669c4e6369a3b5aec0961048b77d9
Luke Chambers <lukenatchambers@gmail.com>
2018-04-29 11:39:01 -0600

    Create crud for schools

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -8,4 +8,28 @@ class Api::V1::SchoolsController < ApplicationController
     institution = Institution.find(params[:id])
     render json: InstitutionRequestService.new(params).show(institution)
   end
+
+  def create
+    institution = Institution.new(institution_params)
+    if institution.save
+      render json: success_message
+    else
+      render json: failure_message
+    end
+  end
+
+  private
+
+    def institution_params
+      params.require(:school).permit(:name)
+    end
+
+    def success_message
+      { :message => "Successfully Created" }
+    end
+
+    def failure_message
+      { :message => "Something went wrong. Please try again" }
+    end
+    
 end
